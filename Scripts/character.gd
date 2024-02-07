17f31fb854d6511836efa9cb037a8a64b942586d
Access547 <86067971+Access547@users.noreply.github.com>
2024-02-07 11:56:50 +0000

    Added a visual grid as well as character class
    Placeholder art added for the grid
    
    Character class was added, currently just a sprite 2d with some exportable info
    
    Added a character array to the combat level that holds the player's party.
    
    Added function to construct grid when scene loads, currently just adds characters to their grid position and moves them to it (position is based on marker2ds)

---
+++ Scripts/character.gd
@@ -0,0 +1,5 @@
+extends Sprite2D
+class_name Character
+
+
+@export var gridPos: Array[int]
--- Scripts/combat_level.gd
+++ Scripts/combat_level.gd
@@ -4,7 +4,7 @@ var gridInfo = {"character": null,
 				"targeted": true,
 				"effect": null}
 
-
+@export var characters: Array[Character]
 
 #  0   1
 #╔═══╦═══╗
@@ -18,7 +18,7 @@ var gridInfo = {"character": null,
 #╚═══╩═══╝
 #Below is the grid variable, which is an array that contains 4 2 sized arrays, all containing the above
 #'gridInfo' dictionary. To reference a specific grid space, follow the chart above. AKA to get info about tile 
-#1,2, you would say grid[1][2]["key"]
+#1,2, you would write grid[1][2]["key"]
 
 
 var grid =	 [[gridInfo.duplicate(false), gridInfo.duplicate(),
@@ -28,8 +28,28 @@ var grid =	 [[gridInfo.duplicate(false), gridInfo.duplicate(),
 
 
 
-func _ready():
-	grid[0][0]["targeted"] = false
-	
-	print (grid[0][0]["targeted"])
-	print(grid[0][1]["targeted"])
+func _ready() -> void: 
+	ConstructGrid()
+
+
+
+#A function to get the position of the grid, perhaps a bit cleaner than typing grid[x][y][key]
+func getGridPos(x: int, y: int, key: String):
+	return grid[x][y][key]
+
+#A function to set the position of a tile in the grid, perhaps a bit cleaner than typing grid[x][y][key] = dwa
+func setGridPos(x: int, y: int, key: String, value):
+	grid[x][y][key] = value
+
+
+#Function used to construct the grid at combat start.
+func ConstructGrid() -> void:
+	#Loops through all characters to place them
+	for i in characters.size():
+		#Update grid to contain character
+		setGridPos(characters[i].gridPos[0], characters[i].gridPos[1], "character", characters[i])
+		MoveCharacter(i) #Moves current character
+
+
+func MoveCharacter(character) -> void:
+	characters[character].position = get_node(str(characters[character].gridPos[0], characters[character].gridPos[1])).position
